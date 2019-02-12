75b365d4b039a820048a8f9cf86e23cdacaed6b2
Can Elkatmış <cane@netas.com.tr>
2019-02-12 10:57:36 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@
 if [ $# -eq 0 ] || [ "$1" == "-?" ] || [ "$1" == "--help" ] || [ "$1" == "-h" ]
   then
     echo "Usage: `basename $0` <BITBUCKET_URL> <USER_NAME> <PROJECT_NAME> [<LOCAL_FOLDER>]"
-    echo "e.g. `basename $0` somewhere.mybitbucketserver.com some_user some_project"
+    echo "e.g. `basename $0` mybitbucketserver.com some_user some_project"
     exit 1
 fi
 
